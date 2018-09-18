06c589f4314f3403e7b2d9823f558aabf03c6990
Pierre-Hugues Husson <pierre-hugues.husson@softathome.com>
2018-09-18 16:28:34 +0200

    Declare framework-side hostapd (will be done by treble app)
    Conflicts:
    	board-base.mk
    
    Change-Id: I5f4d3912ddf03736cdf90ed8efdcfce135ae23a6

--- board-base.mk
+++ board-base.mk
@@ -1,4 +1,6 @@
 TARGET_SYSTEM_PROP := device/phh/treble/system.prop $(TARGET_SYSTEM_PROP)
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/phh/treble/bluetooth
 TARGET_EXFAT_DRIVER := exfat
+DEVICE_FRAMEWORK_MANIFEST_FILE := device/phh/treble/framework_manifest.xml
+
 BLISS_TREBLE_BUILD := true
