3c0442b18f65c065636b4b10653df00bbd3609ba
Jon West <electrikjesus@gmail.com>
2019-05-11 19:36:45 -0400

    x86/x86_64 support updates

--- phhgsi_x86_64_ab/BoardConfig.mk
+++ phhgsi_x86_64_ab/BoardConfig.mk
@@ -1,4 +1,4 @@
-include build/make/target/board/generic_x86_64/BoardConfig.mk
+include build/make/target/board/generic_x86_64_ab/BoardConfig.mk
 include device/phh/treble/board-base.mk
 
 ifeq ($(BOARD_SYSTEMIMAGE_PARTITION_RESERVED_SIZE),)
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -46,3 +46,39 @@ add_lunch_combo treble_arm_boN-userdebug
 add_lunch_combo treble_arm_boS-userdebug
 add_lunch_combo treble_arm_bvN-userdebug
 add_lunch_combo treble_arm_bvS-userdebug
+
+add_lunch_combo treble_x86_afN-userdebug
+add_lunch_combo treble_x86_afS-userdebug
+add_lunch_combo treble_x86_agN-userdebug
+add_lunch_combo treble_x86_agS-userdebug
+add_lunch_combo treble_x86_aoN-userdebug
+add_lunch_combo treble_x86_aoS-userdebug
+add_lunch_combo treble_x86_avN-userdebug
+add_lunch_combo treble_x86_avS-userdebug
+
+add_lunch_combo treble_x86_bfN-userdebug
+add_lunch_combo treble_x86_bfS-userdebug
+add_lunch_combo treble_x86_bgN-userdebug
+add_lunch_combo treble_x86_bgS-userdebug
+add_lunch_combo treble_x86_boN-userdebug
+add_lunch_combo treble_x86_boS-userdebug
+add_lunch_combo treble_x86_bvN-userdebug
+add_lunch_combo treble_x86_bvS-userdebug
+
+add_lunch_combo treble_x86_64_afN-userdebug
+add_lunch_combo treble_x86_64_afS-userdebug
+add_lunch_combo treble_x86_64_agN-userdebug
+add_lunch_combo treble_x86_64_agS-userdebug
+add_lunch_combo treble_x86_64_aoN-userdebug
+add_lunch_combo treble_x86_64_aoS-userdebug
+add_lunch_combo treble_x86_64_avN-userdebug
+add_lunch_combo treble_x86_64_avS-userdebug
+
+add_lunch_combo treble_x86_64_bfN-userdebug
+add_lunch_combo treble_x86_64_bfS-userdebug
+add_lunch_combo treble_x86_64_bgN-userdebug
+add_lunch_combo treble_x86_64_bgS-userdebug
+add_lunch_combo treble_x86_64_boN-userdebug
+add_lunch_combo treble_x86_64_boS-userdebug
+add_lunch_combo treble_x86_64_bvN-userdebug
+add_lunch_combo treble_x86_64_bvS-userdebug
